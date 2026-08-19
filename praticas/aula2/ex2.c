4bce5f50d23f1f753212fbef63ac058a0e55957b
unknown <2622130002@ACADEMICO.IESB.BR>
2026-08-19 10:37:38 -0300

    Finalizando aula2

---
+++ praticas/aula2/ex2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    //testando erros
+    printf("Pedro Henrique\n");
+    //testando erros
+    printf("Algoritmos e programaçao de computadores\n");
+    return 0;
+}
