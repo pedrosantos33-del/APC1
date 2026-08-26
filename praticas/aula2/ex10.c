8c74f511cab61162a744e4d846d14b1dd72b59ab
unknown <2622130002@academico.iesb.br>
2026-08-26 10:10:23 -0300

    finalizando aula 3

--- praticas/aula2/ex10.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#define IDADE_MINIMA 0
-
-int main() {
-    int idade = -5;
-    printf("idade %d\n", idade);
-    printf("idade minima %d\n", IDADE_MINIMA);
-    return 0;
-}
--- praticas/aula2/ex6.c
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    // Char apenas aceita um caractere, diferente de string que aceita uma sequência de caracteres.
-    char letra = 'A';
-    return 0;
