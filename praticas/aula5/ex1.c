0c7f084cb7f5985e079d0995c98b9259d35d88a1
pedrosantos33-del <pedro.santos33@iesb.edu.br>
2026-09-01 19:44:57 -0300

    atualizando localmente

---
+++ praticas/aula5/ex1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main () {
+    int a = 17;
+    int b = 5;
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+    return 0;
+}
